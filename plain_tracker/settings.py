7ff71f971ed78b299d26a9767ab9963cd2cbfca1
rahimi-mohd <reimy94@gmail.com>
2025-08-27 14:55:13 +0800

    custom user model

--- plain_tracker/settings.py
+++ plain_tracker/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
