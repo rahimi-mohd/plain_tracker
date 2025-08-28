f5319274145d4a2aec58f395f3aaa483f6e4813e
rahimi-mohd <reimy94@gmail.com>
2025-08-28 14:14:07 +0800

    permission added

--- trackers/views.py
+++ trackers/views.py
@@ -1,34 +1,43 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 
 from .models import Tracker
 
 
 # Create your views here.
-class TrackerListView(ListView):
+class TrackerListView(LoginRequiredMixin, ListView):
     model = Tracker
     template_name = "trackers/tracker_list.html"
 
 
-class TrackerDetailView(DetailView):
+class TrackerDetailView(LoginRequiredMixin, DetailView):
     model = Tracker
     template_name = "trackers/tracker_detail.html"
 
 
-class TrackerUpdateView(UpdateView):
+class TrackerUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Tracker
     fields = ("body", "priority", "status", "assigned_to")
     template_name = "trackers/tracker_edit.html"
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class TrackerDeleteView(DeleteView):
+
+class TrackerDeleteView(LoginRequiredMixin, DeleteView):
     model = Tracker
     template_name = "trackers/tracker_delete.html"
     success_url = reverse_lazy("tracker_list")
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class TrackerCreateView(CreateView):
+
+class TrackerCreateView(LoginRequiredMixin, CreateView):
     model = Tracker
     template_name = "trackers/tracker_new.html"
     fields = (
