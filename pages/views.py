62cdda320e97d5568e57081bb1cf1dafdd400922
kerengge <reimy94@gmail.com>
2025-09-01 20:56:21 +0800

    add about page

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
